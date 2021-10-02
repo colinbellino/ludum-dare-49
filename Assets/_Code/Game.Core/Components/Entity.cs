689ee173c8c7b225cca98a3c5f3b8f438232a778
Colin Bellino <bellino.colin@gmail.com>
2021-10-02 13:48:06 +0200

    Add smooth movement (lerp) between cells

--- Assets/_Code/Game.Core/Components/Entity.cs
+++ Assets/_Code/Game.Core/Components/Entity.cs
@@ -9,19 +9,28 @@ namespace Game.Core
 		[SerializeField] public SpriteRenderer SpriteRenderer;
 		[SerializeField] public AudioSource AudioSource;
 
+		[Header("AI")]
 		[SerializeField] public bool ControlledByPlayer;
+		[SerializeField] public bool MoveTowardsPlayer;
 
+		[HideInInspector] public bool Placed;
 		[HideInInspector] public Vector3Int GridPosition;
 
+		[Header("Movement")]
+		[HideInInspector] public bool Moving;
+		[SerializeField] public float MoveSpeed = 4f;
+		[HideInInspector] public float MoveT;
+		[HideInInspector] public float MoveStartTimestamp;
+
+		[Header("Anger")]
 		[SerializeField] public bool AffectedByAnger;
 		[HideInInspector] public int AngerProgress;
 		[SerializeField] public AngerStates AngerState;
 
+		[Header("Triggers")]
 		[SerializeField] public bool Trigger;
 		[SerializeField] public AngerStates TriggerState;
 		[SerializeField] public TriggerActions TriggerAction;
-
-		[SerializeField] public bool MoveTowardsPlayer;
 	}
 
 	[Flags] public enum AngerStates { None, Calm, Angry }
