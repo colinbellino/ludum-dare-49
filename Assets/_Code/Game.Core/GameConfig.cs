90896015cf5555fc171db593c6f5cd13b6e053e3
Colin Bellino <me@colinbellino.com>
2021-10-08 01:36:30 +0200

    Add a quick and dirty way of taking screenshots of the levels

--- Assets/_Code/Game.Core/GameConfig.cs
+++ Assets/_Code/Game.Core/GameConfig.cs
@@ -12,6 +12,9 @@ namespace Game.Core
 		public bool DebugFSM;
 		public int LockFPS = 60;
 
+		[Header("Replay")]
+		public bool TakeScreenshots;
+
 		[Header("Level")]
 		public Level[] AllLevels;
 
