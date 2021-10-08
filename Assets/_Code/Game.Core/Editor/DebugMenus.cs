5c033703824c94480591ffb197dab9bd237b137b
Colin Bellino <bellino.colin@gmail.com>
2021-10-08 19:15:53 +0200

    Associate level screenshots to the level asset

--- Assets/_Code/Game.Core/Editor/DebugMenus.cs
+++ Assets/_Code/Game.Core/Editor/DebugMenus.cs
@@ -1,4 +1,4 @@
-using System.IO;
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 
@@ -6,13 +6,13 @@ namespace Game.Core.Editor
 {
 	public class DebugMenus
 	{
-		[MenuItem("Alteration/Merge level assets")]
-		static void DoSomething()
+		[MenuItem("Alteration/Associate screenshots to levels")]
+		static void AssociateScreenshots()
 		{
 			// var path = Path.Combine(Application.persistentDataPath);
 			// EditorUtility.RevealInFinder(path);
 
-			UnityEngine.Debug.Log("Merging level assets.");
+			UnityEngine.Debug.Log("Associating screenshots to levels.");
 
 			var config = Resources.Load<GameConfig>("Game Config");
 
@@ -21,44 +21,23 @@ namespace Game.Core.Editor
 				var level = config.AllLevels[i];
 
 				var originalScreenshot = AssetDatabase.LoadAssetAtPath<Texture2D>($"Assets/Resources/Levels/{i + 1:D2}.png");
-				var screenshot = new Texture2D(originalScreenshot.width, originalScreenshot.height, originalScreenshot.format, true);
-				Graphics.CopyTexture(originalScreenshot, screenshot);
-				screenshot.name = "Screenshot";
-				if (level.Screenshot != null)
+				if (originalScreenshot)
 				{
-					level.Screenshot = screenshot;
-				}
-				else
-				{
-					AssetDatabase.AddObjectToAsset(screenshot, level);
-					level.Screenshot = screenshot;
-					AssetDatabase.DeleteAsset($"Assets/Resources/Levels/{i + 1:D2}.png");
+					level.Screenshot = originalScreenshot;
 				}
 
-				// var originalTrace = AssetDatabase.LoadAssetAtPath<TextAsset>($"Assets/Resources/Levels/{i + 1:D2}.inputtrace");
-				// UnityEngine.Debug.Log(originalTrace + " " + $"Assets/Resources/Levels/{i + 1:D2}.inputtrace");
-				// var trace = new TextAsset();
-				// var stream = new MemoryStream(originalTrace.bytes);
-				// var reader = new StreamReader(stream);
-				// // trace.text = reader.ReadToEnd();
-
-				// // File.WriteAllText(UnityEditor.AssetDatabase.GetAssetPath(trace), originalTrace.text);
-				// // trace.bytes = originalTrace.bytes;
-				// trace.name = "Input Trace";
-				// if (level.InputTrace)
-				// {
-				// 	level.InputTrace = trace;
-				// }
-				// else
-				// {
-				// 	AssetDatabase.AddObjectToAsset(trace, level);
-				// 	level.InputTrace = trace;
-				// 	// UnityEditor.AssetDatabase.DeleteAsset("Assets/Resources/Levels/01.png");
-				// }
+				PrefabUtility.SavePrefabAsset(level.gameObject);
 			}
 
 			UnityEditor.AssetDatabase.SaveAssets();
 			UnityEditor.AssetDatabase.Refresh();
 		}
+
+		[MenuItem("Alteration/Associate screenshots to levels", true)]
+		static bool ValidateAssociateScreenshots()
+		{
+			return Application.isPlaying == false;
+		}
 	}
 }
+#endif
