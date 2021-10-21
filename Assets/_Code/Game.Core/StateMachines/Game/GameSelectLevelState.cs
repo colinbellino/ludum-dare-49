d20a64cf5f06ae31a8140272be2680e1dcf1a1c9
Colin Bellino <me@colinbellino.com>
2021-10-22 00:02:56 +0200

    Move level select to its own game state

---
+++ Assets/_Code/Game.Core/StateMachines/Game/GameSelectLevelState.cs
@@ -0,0 +1,56 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using FMOD.Studio;
+using UnityEngine;
+
+namespace Game.Core.StateMachines.Game
+{
+	public class GameSelectLevelState : BaseGameState
+	{
+		public GameSelectLevelState(GameFSM fsm, GameSingleton game) : base(fsm, game) { }
+
+		public override async UniTask Enter()
+		{
+			await base.Enter();
+
+			await _ui.ShowLevelSelection(0);
+			for (int i = 0; i < _ui.LevelButtons.Length; i++)
+			{
+				var button = _ui.LevelButtons[i];
+				int levelIndex = i;
+				button.onClick.AddListener(() => LoadLevel(levelIndex));
+			}
+
+			await _ui.FadeOut();
+		}
+
+		public override void Tick()
+		{
+			if (_controls.Global.Cancel.WasPerformedThisFrame())
+			{
+				_fsm.Fire(GameFSM.Triggers.Quit);
+			}
+		}
+
+		public override async UniTask Exit()
+		{
+			for (int i = 0; i < _ui.LevelButtons.Length; i++)
+			{
+				var button = _ui.LevelButtons[i];
+				int levelIndex = i;
+				button.onClick.RemoveListener(() => LoadLevel(levelIndex));
+			}
+
+			await _ui.FadeIn(Color.black);
+			await _ui.HideLevelSelection(0);
+		}
+
+		private async void LoadLevel(int levelIndex)
+		{
+			Debug.Log($"Loading level {levelIndex}.");
+			_state.CurrentLevelIndex = levelIndex;
+			await _ui.FadeIn(Color.black);
+			_fsm.Fire(GameFSM.Triggers.LevelSelected);
+		}
+	}
+}
